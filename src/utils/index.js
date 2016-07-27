42ada3428c05c8cd95983b379b33c072c9847416
hammadj <jutt@ualberta.ca>
2016-07-26 18:04:19 -0700

    remove testUtils from utils import to fix webpack error
    textUtils imports enzyme which is only a dev dependency, shouldn’t be
    in utils which is used in the production code

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,12 +4,10 @@
 import getBaseStyle, { getColor } from './getBaseStyle'
 import getTextStyle from './getTextStyle'
 import mapToProps from './mapPropsToStyles'
-import * as testUtils from './testUtils'
 
 export {
   getBaseStyle,
   getColor,
   getTextStyle,
-  mapToProps,
-  testUtils
+  mapToProps
 }
