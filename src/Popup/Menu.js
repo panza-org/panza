333f39d861a38ddf282bea457d714d7c8d53a60e
bmcmahen <ben.mcmahen@gmail.com>
2016-09-02 16:10:52 -0700

    make some small tweaks to Menu

--- src/Popup/Menu.js
+++ src/Popup/Menu.js
@@ -3,24 +3,28 @@ import React, { PropTypes } from 'react'
 import {
   Modal,
   Platform,
+  View,
+  StyleSheet,
   TouchableHighlight,
   TouchableWithoutFeedback
 } from 'react-native'
 import {
   Base,
-  Text,
-  themeProvider
+  Text
 } from '../index'
+import { themeProvider } from '../config'
 
 type OptionsType = {
-
+  onPress: Function;
+  label: string;
 }
 
 type Props = {
   showing: boolean;
   onRequestClose: Function;
-  autoDismiss: boolean;
-  showCancel: boolean;
+  autoDismiss?: boolean;
+  showCancel?: boolean;
+  backgroundColor?: string;
   modalProps?: Object;
   options: ReactElement<any> | Array<OptionsType>;
 }
@@ -34,6 +38,23 @@ export default class Popup extends React.Component {
     autoDismiss: true,
     modalProps: {},
     backgroundColor: 'rgba(0,0,0,0.6)'
+  }
+
+  static propTypes = {
+    showing: PropTypes.bool.isRequired,
+    onRequestClose: PropTypes.func.isRequired,
+    autoDismiss: PropTypes.bool,
+    showCancel: PropTypes.bool,
+    backgroundColor: PropTypes.string,
+    modalProps: PropTypes.object,
+    customHeader: PropTypes.node,
+    options: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.arrayOf(PropTypes.shape({
+        onPress: PropTypes.func.isRequired,
+        label: PropTypes.string.isRequired
+      }))
+    ])
   }
 
   props: Props;
@@ -66,22 +87,22 @@ export default class Popup extends React.Component {
 
     if (showCancel && isArray) {
       els.push(
-       <Option
-         onPress={onRequestClose}
-         opt={{
-           label: 'Cancel',
-           condensed: false,
-           primary: true,
-           underlayColor: '#eee'
-         }}
-         key='cancel'
-       />
+        <Option
+          onPress={onRequestClose}
+          opt={{
+            label: 'Cancel',
+            condensed: false,
+            primary: true,
+            underlayColor: '#eee'
+          }}
+          key='cancel'
+        />
      )
     }
 
     return (
       <Modal
-        animationTyp='fade'
+        animationType='fade'
         transparent
         visible={showing}
         onRequestClose={onRequestClose}
@@ -92,6 +113,7 @@ export default class Popup extends React.Component {
             flex={1}
             backgroundColor={backgroundColor}
             alignSelf='stretch'
+            justify='center'
           >
             <Base
               backgroundColor='white'
@@ -149,7 +171,7 @@ const PopupMenuOption = ({
       }}
       {...other}
     >
-      <Base justify='center'>
+      <View style={{ justifyContent: 'center' }}>
         <Text
           bold={primary}
           small={condensed}
@@ -160,7 +182,7 @@ const PopupMenuOption = ({
         >
           {label}
         </Text>
-      </Base>
+      </View>
     </Base>
   )
 }
